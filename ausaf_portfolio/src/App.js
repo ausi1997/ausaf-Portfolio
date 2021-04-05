509e6b071205d71d01c6b7a8e68f4777e8557843
ausi1997.com <ausafen1018@gmail.com>
2021-04-05 16:53:27 +0530

    tech skills page

--- ausaf_portfolio/src/App.js
+++ ausaf_portfolio/src/App.js
@@ -3,18 +3,30 @@ import NavBar from "./components/NavBar";
 import Home from "./components/home/home.";
 import Title from "./components/title/title";
 import AboutMe from "./components/about/about";
+import Skills from "./components/skills/skills";
 
-//import Container from "react-bootstrap/Container";
+import Container from "react-bootstrap/Container";
 //import {Parallax , Background} from "react-parallax";
 //import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide"
 function App() {
   return (
     <div className="App" style={{ position: "relative" }}>
     <NavBar></NavBar>
     <Home></Home>
     <Title></Title>
-    <AboutMe></AboutMe> 
-    
+    <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <AboutMe></AboutMe>
+        </Slide>
+      </Container>
+    <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <Skills />
+        </Slide>
+      </Container>
     </div>
   );
 }
---
+++ ausaf_portfolio/src/components/skills/skills-data.js
@@ -0,0 +1,138 @@
+import HTML5 from "../../assets/skills/html-5.svg";
+import CSS3 from "../../assets/skills/css3.svg";
+import BOOTSTRAP from "../../assets/skills/bootstrap-4.svg";
+import REACT from "../../assets/skills/react.svg";
+import REACT_ROUTER from "../../assets/skills/react-router.svg";
+import MATERIALUI from "../../assets/skills/material-ui-1.svg";
+import REACT_BOOTSTRAP from "../../assets/skills/react-bootstrap.svg";
+import STYLED_COMPONENTS from "../../assets/skills/styled-components.svg";
+import NODE_JS from "../../assets/skills/nodejs.svg";
+import EXPRESS from "../../assets/skills/express.svg";
+import MONGODB from "../../assets/skills/mongodb.svg";
+import POSTGRESQL from "../../assets/skills/postgresql.svg";
+import GIT from "../../assets/skills/git-icon.svg";
+import HEROKU from "../../assets/skills/heroku.svg";
+import GITHUB_PAGES from "../../assets/skills/github.svg";
+import JAVASCRIPT from "../../assets/skills/javascript.svg";
+
+
+export const skills = {
+    frontend: [
+      {
+        link: "https://en.wikipedia.org/wiki/HTML5",
+        imgAltText: "HTML 5",
+        imgSrc: HTML5,
+        skillName: "HTML5",
+      },
+      {
+        link: "https://medium.com/beginners-guide-to-mobile-web-development/whats-new-in-css-3-dcd7fa6122e1",
+        imgAltText: "CSS 3",
+        imgSrc: CSS3,
+        skillName: "CSS3",
+      },
+      {
+        link: "https://www.javascript.com/",
+        imgAltText: "JavaScript",
+        imgSrc: JAVASCRIPT,
+        skillName: "JavaScript",
+  },    
+      {
+        link: "https://getbootstrap.com/",
+        imgAltText: "Bootstrap",
+        imgSrc: BOOTSTRAP,
+        skillName: "Bootstrap",
+      },
+      {
+        link: "https://reactjs.org/",
+        imgAltText: "React JS",
+        imgSrc: REACT,
+        skillName: "React JS",
+      },
+      {
+        link: "https://reacttraining.com/react-router/",
+        imgAltText: "React Router",
+        imgSrc: REACT_ROUTER,
+        skillName: "React Router",
+      },
+      {
+        link: "https://material-ui.com/",
+        imgAltText: "Material-UI",
+        imgSrc: MATERIALUI,
+        skillName: "Material-UI",
+      },
+      {
+        link: "https://react-bootstrap.github.io/",
+        imgAltText: "React Bootstrap",
+        imgSrc: REACT_BOOTSTRAP,
+        skillName: "React Bootstrap",
+      },
+      {
+        link: "https://styled-components.com/",
+        imgAltText: "styled-components",
+        imgSrc: STYLED_COMPONENTS,
+        skillName: "styled-components",
+      },
+      
+    ],
+  
+    backend: [
+      {
+        link: "https://nodejs.org/en/",
+        imgAltText: "Node.js",
+        imgSrc: NODE_JS,
+        skillName: "Node.js",
+      },
+      {
+        link: "https://expressjs.com/",
+        imgAltText: "Express",
+        imgSrc: EXPRESS,
+        skillName: "Express",
+      }
+    ],
+    hostingPlatforms: [
+      {
+        link: "https://www.heroku.com/",
+        imgAltText: "Heroku",
+        imgSrc: HEROKU,
+        skillName: "Heroku",
+      },
+      {
+        link: "https://pages.github.com/",
+        imgAltText: "GitHub Pages",
+        imgSrc: GITHUB_PAGES,
+        skillName: "GitHub Pages",
+      },
+    ],
+    programmingLanguages: [
+      {
+        link: "https://www.javascript.com/",
+        imgAltText: "JavaScript",
+        imgSrc: JAVASCRIPT,
+        skillName: "JavaScript",
+      }
+      
+    ],
+    databases: [
+      {
+        link: "https://www.postgresql.org/",
+        imgAltText: "PostgreSQL",
+        imgSrc: POSTGRESQL,
+        skillName: "PostgreSQL",
+      },
+      
+      {
+        link: "https://www.mongodb.com/",
+        imgAltText: "MongoDB",
+        imgSrc: MONGODB,
+        skillName: "MongoDB",
+      },
+    ],
+    versionControl: [
+      {
+        link: "https://git-scm.com/",
+        imgAltText: "GIT",
+        imgSrc: GIT,
+        skillName: "GIT",
+      },
+    ],
+  };
